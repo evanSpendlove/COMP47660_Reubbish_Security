bec03f6933a60a950b6f61dc5ee475fef0776527
Reuben Mulligan <reubsmull@gmail.com>
2022-01-30 14:44:22 +0000

    added initial maven and spring setup, added .gitignore for java and maven as well

---
+++ vaccine_registration_site/src/main/java/Reubbish_Security/vaccine_registration_site/VaccineRegistrationSiteApplication.java
@@ -0,0 +1,13 @@
+package Reubbish_Security.vaccine_registration_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineRegistrationSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineRegistrationSiteApplication.class, args);
+	}
+
+}
