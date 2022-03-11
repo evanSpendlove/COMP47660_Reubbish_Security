746daae941a042c18801ed452962ca2cd073f30f
John <odonnell88j@gmail.com>
2022-03-11 17:41:44 +0000

    UserRepository: Added queries for stats controller

--- vaccine_registration_site/src/main/java/com/reubbishsecurity/CovidVaccinations/authentication/repository/UserRepository.java
+++ vaccine_registration_site/src/main/java/com/reubbishsecurity/CovidVaccinations/authentication/repository/UserRepository.java
@@ -1,12 +1,20 @@
 package com.reubbishsecurity.CovidVaccinations.authentication.repository;
 
 import com.reubbishsecurity.CovidVaccinations.authentication.entity.User;
+import com.reubbishsecurity.CovidVaccinations.authentication.entity.util.Nationality;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByPps(String pps_number);
+
+    long countAllByNationalityAndLastactivity(Nationality nationality, User.LastActivity lastActivity);
+
+    long countAllByGenderAndLastactivity(User.Gender gender, User.LastActivity lastActivity);
+
+    long countAllByLastactivityAndDobBetween(User.LastActivity last_Activity, Date rangeLow, Date rangeHigh);
 }
